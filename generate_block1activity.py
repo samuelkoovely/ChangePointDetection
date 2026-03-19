34641d9c778ee75aae06a7018f9478115b042d4a
Samuel Koovely <samuelkoovely@gmail.com>
2026-03-19 23:34:08 +0100

    generate training sets python versions
    removed notebooks

---
+++ generate_block1activity.py
@@ -0,0 +1,38 @@
+import EDLDE
+import random as rd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+tnets = []
+
+n_per_group = 50
+n_groups = 2
+t_start = 0
+t_end = 200
+
+basis_num_communities = 2
+powers_num_communities = [1, 0]
+list_p_within_community = [45/50] * len(powers_num_communities)
+
+inter_tau = 2
+density = 100
+
+
+rd.seed(34)
+for i in range(5):
+    #t_split = rd.randint(50, 150)
+    n1, s1, e1 = EDLDE.EDLDE(
+        density=density, inter_tau=inter_tau, t_start=t_start, t_end=t_end, seed=1415+i)
+
+    tnet = EDLDE.generate_smooth_SBM(inter_tau =  0, density = 0,
+                          n_per_group = n_per_group, n_groups =n_groups,
+                          t_start = t_start, t_end =t_end,
+                          basis_num_communities = basis_num_communities, powers_num_communities = powers_num_communities, list_p_within_community = list_p_within_community,
+                          number_of_events = n1, starting_times=s1, ending_times=e1, seed=271+i)
+
+    tnet_trimmed, t_0 = EDLDE.trim_temporal_network_head_tail(tnet, density=density, inter_tau=inter_tau, tail_start_time=t_end)
+
+    tnets.append({'tnet': tnet_trimmed, 'bkp': (t_end - t_start) / 2 - t_0, 'starts': tnet_trimmed.events_table['starting_times'], 'ends': tnet_trimmed.events_table['ending_times']}) 
+
+pickle.dump(tnets, open('block1activity.pkl', 'wb'))
