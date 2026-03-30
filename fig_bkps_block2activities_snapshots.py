339b122a021b63af238d60552f7e9797a4991ea1
Samuel Koovely <samuelkoovely@gmail.com>
2026-03-30 16:26:27 +0200

    Update fig_bkps_block2activities_snapshots.py
    removed linear approximation

--- fig_bkps_block2activities_snapshots.py
+++ fig_bkps_block2activities_snapshots.py
@@ -5,9 +5,6 @@ import matplotlib.pyplot as plt
 with open('gridsearch_results/block2activities_snapshots/gridsearch_results.pkl', 'rb') as handle:
     results_full = pickle.load(handle)
 
-with open('gridsearch_results/block2activities_snapshots_lin_approx/gridsearch_results.pkl', 'rb') as handle:
-    results_lin = pickle.load(handle)
-
 def get_best_signal_metadata(results):
     lambdas = results['lambdas']
     windows = results['windows']
@@ -17,9 +14,8 @@ def get_best_signal_metadata(results):
     return lamda, window, predicted_change_points
 
 lamda_full, window_full, predicted_change_points_full = get_best_signal_metadata(results_full)
-lamda_lin, window_lin, predicted_change_points_lin = get_best_signal_metadata(results_lin)
 
-fig, axes = plt.subplots(5, 2, figsize=(18, 10), sharex=False, squeeze=False)
+fig, axes = plt.subplots(5, 1, figsize=(10, 10), sharex=False, squeeze=False)
 for sample in range(5):
     with open('data/block2activities_snapshots.pkl', 'rb') as handle:
         tnet = pickle.load(handle)[sample]
@@ -31,13 +27,6 @@ for sample in range(5):
         'rb',
     ) as handle:
         signal_full = pickle.load(handle)
-
-    with open(
-        f'gridsearch_results/block2activities_snapshots_lin_approx/signals/sample_{sample}/'
-        f'signal_lamda_{lamda_lin:.11f}_window_{window_lin:g}.pkl',
-        'rb',
-    ) as handle:
-        signal_lin = pickle.load(handle)
 
     ax_full = axes[sample, 0]
     ax_full.plot(signal_full['signal'], label=f'sample_{sample}')
@@ -61,35 +50,10 @@ for sample in range(5):
     ax_full.legend(loc='upper right')
     ax_full.set_ylabel(f's{sample}')
 
-    ax_lin = axes[sample, 1]
-    ax_lin.plot(signal_lin['signal'], label=f'sample_{sample}')
-    ymin_lin = np.min(signal_lin['signal'])
-    ymax_lin = np.max(signal_lin['signal'])
-    for bkp in bkps:
-        ax_lin.vlines(
-            bkp,
-            ymin=ymin_lin,
-            ymax=ymax_lin,
-            color='black',
-        )
-    for pred_cp in predicted_change_points_lin[sample]:
-        ax_lin.vlines(
-            pred_cp,
-            ymin=ymin_lin,
-            ymax=ymax_lin,
-            linestyles='dashed',
-            color='red',
-        )
-    ax_lin.legend(loc='upper right')
-
 axes[0, 0].set_title(
     f'Original inter-T\nlambda={lamda_full:.5g}, window={window_full:g}'
 )
-axes[0, 1].set_title(
-    f'Linear approximation\nlambda={lamda_lin:.5g}, window={window_lin:g}'
-)
 axes[-1, 0].set_xlabel('time')
-axes[-1, 1].set_xlabel('time')
 fig.tight_layout()
 #plt.savefig('/home/b/skoove/Desktop/ChangePointDetection/fig_bkps_block2activities_snapshots.pdf', format='pdf', dpi=300, bbox_inches='tight')
 plt.show()
